9bbf881552d1ff9dd8a4d29257049aeb0587d4f7
luka1104 <skyward.luke11@gmail.com>
2021-12-16 22:03:55 +0900

    ローカル環境

--- db/migrate/20211213082632_change_column_to_project_pvs.rb
+++ db/migrate/20211213082632_change_column_to_project_pvs.rb
@@ -1,6 +1,5 @@
 class ChangeColumnToProjectPvs < ActiveRecord::Migration[6.1]
   def change
-    remove_column :project_pvs, :user_ip, :integer
     add_column :project_pvs, :user_ip, :string
   end
 end
