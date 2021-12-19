ac5e6c3fb8e3ffde63448cad482237b17e38cb01
luka1104 <skyward.luke11@gmail.com>
2021-12-20 04:12:46 +0900

    production.rb変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,15 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.default_url_options = { host: 'adonation.app' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :user_name => 'skyward.luke11@gmail.com',
+    :password => 'xclv jkfo fmmx mpzm',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
