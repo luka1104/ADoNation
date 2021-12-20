80e3236c0eedb03a1decb85d2688dedfaa754817
中田誠人 <masat@MacBook-Pro-4.local>
2021-12-21 05:05:56 +0900

    stash完了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.active_storage.service = :local
   # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
+  #config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
