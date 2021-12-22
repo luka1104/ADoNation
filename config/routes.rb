11131d084289c8254b30db69cbb8a410f972b30e
中田誠人 <masat@MacBook-Pro-4.local>
2021-12-23 05:00:52 +0900

    管理者画面作成&プロジェクト管理実装

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,11 @@ Rails.application.routes.draw do
     end
     resources :project_pvs
   end
+
+  resources :admin, only: [:index] do
+    member do
+      put :active
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_22_050005) do
+ActiveRecord::Schema.define(version: 2021_12_22_182738) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2021_12_22_050005) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "status", default: false, null: false
   end
 
   create_table "sns_credentials", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
